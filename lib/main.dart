50e3857ff7f8e6381055648fbb38685bbd97edcb
Fidelis Okeke <padrefidelis111@gmail.com>
2023-02-08 09:22:25 -0800

    create the full app

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+      home: Scaffold(
+    appBar: AppBar(
+      title: const Text("I am Rich"),
+      backgroundColor: Colors.blueGrey[900],
+    ),
+    backgroundColor: Colors.blueGrey,
+    body: const Center(
+        child: Image(
+      image: AssetImage('images/diamond.png'),
+    )),
+  )));
+}
